603b5265ca14b15d1f45a2ddc569dc74328ce215
YEXIAONAN <yexiaonanair@outlook.com>
2025-01-03 14:25:47 +0800

    modified:   Script/DeleteScript.sh

--- Script/DeleteScript.sh
+++ Script/DeleteScript.sh
@@ -19,3 +19,16 @@ cat <<EOF
 脚本已在GitHub开源,链接 https://github.com/YEXIAONAN/AutomaticScript
 ----------------------------------------------------------------
 EOF
+
+# 删除 /opt/ 目录下的 module 文件夹
+echo "正在删除 /opt/module 文件夹..."
+rm -rf /opt/module
+echo "/opt/module 文件夹删除完成。"
+
+# 删除 /etc/profile 文件中的相关环境变量配置
+echo "正在删除 /etc/profile 文件中的环境变量配置..."
+sed -i '/# 添加环境变量到 \/etc\/profile/,+3d' /etc/profile
+echo "/etc/profile 文件中的环境变量配置已删除。"
+
+# 完成
+echo "脚本执行完成，环境已清理干净。"
